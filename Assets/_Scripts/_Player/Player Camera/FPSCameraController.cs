40371b00ac70e98df22d5e477adc9549a4622976
Nir Mula <nirmula3d@gmail.com>
2025-12-28 22:03:04 +0200

    Added:
    Cinemachine
    Odin

---
+++ Assets/_Scripts/_Player/Player Camera/FPSCameraController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Unity.Cinemachine;
+using UnityEngine.InputSystem;
+public class FPSCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
